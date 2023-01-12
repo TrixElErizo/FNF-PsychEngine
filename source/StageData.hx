71dcbc9204c327265b3f177a2cadce83b7a263a3
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2023-01-12 16:12:27 -0300

    Pera que ²

--- source/StageData.hx
+++ source/StageData.hx
@@ -73,7 +73,7 @@ class StageData {
 		var path:String = SUtil.getStorageDirectory() + Paths.getPreloadPath('stages/' + stage + '.json');
 
 		#if MODS_ALLOWED
-		var modPath:String = SUtil.getStorageDirectory() + Paths.modFolders('stages/' + stage + '.json');
+		var modPath:String = Paths.modFolders('stages/' + stage + '.json');
 		if(FileSystem.exists(modPath)) {
 			rawJson = File.getContent(modPath);
 		} else if(FileSystem.exists(path)) {
